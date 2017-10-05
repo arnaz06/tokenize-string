8ba2d23608c059882695cdfb02a3c16ea6dfa063
sikun <arnazadiputra@gmail.com>
2017-10-05 09:36:31 +0700

    dapat menyimpan token dalam bentuk array

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -11,15 +11,11 @@ module.exports = {
     var _token = req.body.string.split(/[\s,]+/)
     var i = 0
     var key = []
-    for (i; i < _token.length; i++) {
-      key.push('string'+i)
-      console.log(key[i]);
-      console.log(_token[i])
-      var token = new Token({
-        key[i] : _token[i],
+    var token = new Token({
+        key : _token
 
-      })
-    }
+    })
+
     token.save(function(err){
       if(err){
         console.log(err)
